2ed9db04003b558ed1bb4de82f60b373aacc10c5
Valentino Behret <valentino.b.behret@partner.bmw.de>
2020-10-28 14:05:54 +0100

    master: Finishin pre-processing

--- CarND-Traffic-Sign-Classifier-Project/__main__.py
+++ CarND-Traffic-Sign-Classifier-Project/__main__.py
@@ -6,6 +6,19 @@ from sklearn.preprocessing import LabelBinarizer
 from tensorflow.python.ops.variables import Variable
 
 
+def rgb2gray(rgb):
+    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+    return gray
+
+
+def reshape_image_data(image_data):
+    list_gray = []
+    for rgb in image_data:
+        list_gray.append(rgb2gray(rgb).flatten())
+    return np.array(list_gray)
+
+
 def normalize_grayscale(image_data):
     """
     Normalize the image data with Min-Max scaling to a range of [0.1, 0.9]
@@ -38,22 +51,16 @@ def main():
     valid_features, valid_labels = valid['features'], valid['labels']
     test_features, test_labels = test['features'], test['labels']
 
-    n_train = len(train['labels'])
-    n_validation = len(valid['labels'])
-    n_test = len(test['labels'])
-    image_shape = train['features'].shape[1:3]
-    n_classes = len(set(train['labels']))
-
-    print("Number of training examples =", n_train)
-    print("Number of testing examples =", n_test)
-    print("Number of validation examples =", n_validation)
-    print("Image data shape =", image_shape)
-    print("Number of classes =", n_classes)
+    train_features = reshape_image_data(train_features)
+    test_features = reshape_image_data(test_features)
+    valid_features = reshape_image_data(valid_features)
 
     if not is_features_normal:
         train_features = normalize_grayscale(train_features)
         test_features = normalize_grayscale(test_features)
+        valid_features = normalize_grayscale(valid_features)
         is_features_normal = True
+        print("Normalize grayscale done!")
 
     if not is_labels_encod:
         # Turn labels into numbers and apply One-Hot Encoding
@@ -61,39 +68,37 @@ def main():
         encoder.fit(train_labels)
         train_labels = encoder.transform(train_labels)
         test_labels = encoder.transform(test_labels)
+        valid_labels = encoder.transform(valid_labels)
 
         # Change to float32, so it can be multiplied against the features in TensorFlow, which are float32
         train_labels = train_labels.astype(np.float32)
         test_labels = test_labels.astype(np.float32)
+        valid_labels = valid_labels.astype(np.float32)
         is_labels_encod = True
+        print("Encoding done!")
 
-    features_count = 784
-    labels_count = 10
+    n_train = len(train_labels)
+    n_validation = len(valid_labels)
+    n_test = len(test_labels)
+    n_features = train_features.shape[1]
+    n_labels = train_labels.shape[1]
+
+    print("Number of training examples =", n_train)
+    print("Number of testing examples =", n_test)
+    print("Number of validation examples =", n_validation)
+    print("Number of features =", n_features)
+    print("Number of classes =", n_labels)
 
     # Problem 2 - Set the features and labels tensors
     features = tf.placeholder(tf.float32)
     labels = tf.placeholder(tf.float32)
 
     # Problem 2 - Set the weights and biases tensors
-    weights = tf.Variable(tf.truncated_normal((features_count, labels_count)))
-    biases = tf.Variable(tf.zeros(labels_count))
+    weights = tf.Variable(tf.truncated_normal((n_features, n_labels)))
+    biases = tf.Variable(tf.zeros(n_labels))
 
-    assert features._op.name.startswith('Placeholder'), 'features must be a placeholder'
-    assert labels._op.name.startswith('Placeholder'), 'labels must be a placeholder'
     assert isinstance(weights, Variable), 'weights must be a TensorFlow variable'
     assert isinstance(biases, Variable), 'biases must be a TensorFlow variable'
-
-    assert features._shape == None or ( \
-                features._shape.dims[0].value is None and \
-                features._shape.dims[1].value in [None, 784]), 'The shape of features is incorrect'
-    assert labels._shape == None or ( \
-                labels._shape.dims[0].value is None and \
-                labels._shape.dims[1].value in [None, 10]), 'The shape of labels is incorrect'
-    assert weights._variable._shape == (784, 10), 'The shape of weights is incorrect'
-    assert biases._variable._shape == (10), 'The shape of biases is incorrect'
-
-    assert features._dtype == tf.float32, 'features must be type float32'
-    assert labels._dtype == tf.float32, 'labels must be type float32'
 
     # Feed dicts for training, validation, and test session
     train_feed_dict = {features: train_features, labels: train_labels}
@@ -107,10 +112,6 @@ def main():
 
     # Cross entropy
     cross_entropy = -tf.reduce_sum(labels * tf.log(prediction), axis=1)
-
-    # some students have encountered challenges using this function, and have resolved issues
-    # using https://www.tensorflow.org/api_docs/python/tf/nn/softmax_cross_entropy_with_logits
-    # please see this thread for more detail https://discussions.udacity.com/t/accuracy-0-10-in-the-intro-to-tensorflow-lab/272469/9
 
     # Training loss
     loss = tf.reduce_mean(cross_entropy)
