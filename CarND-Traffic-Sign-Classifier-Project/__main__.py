bbf3027aa2914b2e164de4cfeca57bef382bf056
Valentino Behret <valentino.b.behret@partner.bmw.de>
2020-10-29 11:20:30 +0100

    master: Implement main function

--- CarND-Traffic-Sign-Classifier-Project/__main__.py
+++ CarND-Traffic-Sign-Classifier-Project/__main__.py
@@ -11,47 +11,92 @@ from tqdm import tqdm
 
 from traffic_sign_detection.prepare_tensors import PrepareTensors, DataType
 
-training_file = 'data/train.p'
-validation_file = 'data/test.p'
-testing_file = 'data/valid.p'
-
-logging.basicConfig(level=logging.INFO)
-tensors = PrepareTensors(training_file, testing_file, validation_file)
-# tensors.process()
-X_train = tensors.feature[DataType.TRAIN]
-y_train = tensors.label[DataType.TRAIN]
-X_test = tensors.feature[DataType.TEST]
-y_test = tensors.label[DataType.TEST]
-X_valid = tensors.feature[DataType.VALID]
-y_valid = tensors.label[DataType.VALID]
-
-# Total number of images: 51839
-n_train = 34799
-
-n_validation = 12630
-
-n_test = 4410
-
-image_shape = [32, 32]
-
-n_classes = 43
-
-index = random.randint(0, len(X_train))
-image = X_train[index].squeeze()
-
-plt.imshow(image)
-plt.show()
-print(y_train[index])
-
-X_train, y_train = shuffle(X_train, y_train)
-
 EPOCHS = 10
 BATCH_SIZE = 128
 FEATURE = tf.placeholder(tf.float32, (None, 32, 32, 3))
 LABEL = tf.placeholder(tf.int32, None)
 
 
-def le_net_5(x):
+def main():
+    training_file = 'data/train.p'
+    validation_file = 'data/test.p'
+    testing_file = 'data/valid.p'
+
+    logging.basicConfig(level=logging.INFO)
+    tensors = PrepareTensors(training_file, testing_file, validation_file)
+    # tensors.process()
+    X_train = tensors.feature[DataType.TRAIN]
+    y_train = tensors.label[DataType.TRAIN]
+    X_test = tensors.feature[DataType.TEST]
+    y_test = tensors.label[DataType.TEST]
+    X_valid = tensors.feature[DataType.VALID]
+    y_valid = tensors.label[DataType.VALID]
+
+    # Total number of images: 51839
+    n_train = 34799
+
+    n_validation = 12630
+
+    n_test = 4410
+
+    image_shape = [32, 32]
+
+    n_classes = 43
+
+    index = random.randint(0, len(X_train))
+    image = X_train[index].squeeze()
+
+    plt.imshow(image)
+    plt.show()
+    print(y_train[index])
+
+    X_train, y_train = shuffle(X_train, y_train)
+
+    one_shot_y = tf.one_hot(LABEL, 43)
+
+    learning_rate = 0.001
+
+    logits = le_net_5(FEATURE)
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_shot_y, logits=logits)
+    loss_operation = tf.reduce_mean(cross_entropy)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    training_operation = optimizer.minimize(loss_operation)
+
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_shot_y, 1))
+    accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        num_examples = X_train.shape[0]
+
+        batch_count = int(math.ceil(len(X_train) / BATCH_SIZE))
+
+        print("Training...")
+        accuracy = 0.0
+        for i in range(EPOCHS):
+
+            # Progress bar
+            batches_pbar = tqdm(range(batch_count),
+                                desc='Previous Accuracy={:.3f} Epoch {:>2}/{}'.format(accuracy, i + 1, EPOCHS),
+                                unit='batches')
+
+            X_train, y_train = shuffle(X_train, y_train)
+            for batch_i in batches_pbar:
+                batch_start = batch_i * BATCH_SIZE
+                batch_end = batch_start + BATCH_SIZE
+                batch_x = X_train[batch_start:batch_end]
+                batch_y = y_train[batch_start:batch_end]
+                sess.run(training_operation, feed_dict={FEATURE: batch_x, LABEL: batch_y})
+
+            accuracy = evaluate(X_valid, y_valid, FEATURE, LABEL, accuracy_operation)
+
+        print("Final Accuracy = {:.3f}".format(accuracy))
+        saver.save(sess, './lenet')
+        print("Model saved")
+
+
+def le_net_5(tf_features):
     # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
     mu = 0
     sigma = 0.1
@@ -62,7 +107,7 @@ def le_net_5(x):
     B_1 = tf.Variable(tf.zeros(shape=(1, 28, 28, 6)))
     strides = [1, 1, 1, 1]
     padding = 'VALID'
-    layer_1 = tf.nn.conv2d(x, W_1, strides=strides, padding=padding) + B_1
+    layer_1 = tf.nn.conv2d(tf_features, W_1, strides=strides, padding=padding) + B_1
 
     # Activation.
     layer_1 = tf.nn.relu(layer_1)
@@ -120,22 +165,7 @@ def le_net_5(x):
     return logits
 
 
-one_shot_y = tf.one_hot(LABEL, 43)
-
-learning_rate = 0.001
-
-logits = le_net_5(FEATURE)
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_shot_y, logits=logits)
-loss_operation = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
-training_operation = optimizer.minimize(loss_operation)
-
-correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_shot_y, 1))
-accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-saver = tf.train.Saver()
-
-
-def evaluate(valid_features, valid_labels, tf_features, tf_labels):
+def evaluate(valid_features, valid_labels, tf_features, tf_labels, accuracy_operation):
     n_features = len(valid_features)
     total_accuracy = 0
     tmp_sess = tf.get_default_session()
@@ -148,31 +178,5 @@ def evaluate(valid_features, valid_labels, tf_features, tf_labels):
     return total_accuracy / n_features
 
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    num_examples = X_train.shape[0]
-
-    batch_count = int(math.ceil(len(X_train) / BATCH_SIZE))
-
-    print("Training...")
-    accuracy = 0.0
-    for i in range(EPOCHS):
-
-        # Progress bar
-        batches_pbar = tqdm(range(batch_count),
-                            desc='Previous Accuracy={:.3f} Epoch {:>2}/{}'.format(accuracy, i + 1, EPOCHS),
-                            unit='batches')
-
-        X_train, y_train = shuffle(X_train, y_train)
-        for batch_i in batches_pbar:
-            batch_start = batch_i * BATCH_SIZE
-            batch_end = batch_start + BATCH_SIZE
-            batch_x = X_train[batch_start:batch_end]
-            batch_y = y_train[batch_start:batch_end]
-            sess.run(training_operation, feed_dict={FEATURE: batch_x, LABEL: batch_y})
-
-        accuracy = evaluate(X_valid, y_valid, FEATURE, LABEL)
-
-    print("Final Accuracy = {:.3f}".format(accuracy))
-    saver.save(sess, './lenet')
-    print("Model saved")
+if __name__ == "__main__":
+    main()
