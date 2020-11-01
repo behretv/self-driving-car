6b6002c0da07ff48f00afef1b9eceab9ae20a7d7
Valentino Behret <valentino.b.behret@partner.bmw.de>
2020-11-01 20:36:11 +0100

    master: Implement random parameter generator

--- CarND-Traffic-Sign-Classifier-Project/__main__.py
+++ CarND-Traffic-Sign-Classifier-Project/__main__.py
@@ -15,21 +15,25 @@ def main():
     data = DataHandler(files)
     hyper = HyperParameterHandler(files)
 
-    # 2 DNN
-    deep_network = DeepNeuralNetwork(data, hyper)
-    deep_network.generate_network()
-    deep_network.generate_optimizer()
-    deep_network.compute_cost()
+    for i in range(0, 30):
+        logging.info("%d # ITERATION \n\n", i)
+        hyper.next_parameter_set()
 
-    # 3 Run and save session
-    session = SessionHandler(files, data, deep_network, hyper)
-    valid_accuracy = session.train()
-    test_accuracy = session.test()
+        # 2 DNN
+        deep_network = DeepNeuralNetwork(data, hyper)
+        deep_network.generate_network()
+        deep_network.generate_optimizer()
+        deep_network.compute_cost()
 
-    hyper.update(test_accuracy, data.sample_size(DataType.TEST))
-    hyper.update_file_if_accuracy_improved()
-    print("Valid Accuracy = {:.3f}".format(valid_accuracy))
-    print('Test Accuracy = {:.3f}'.format(test_accuracy))
+        # 3 Run and save session
+        session = SessionHandler(files, data, deep_network, hyper)
+        valid_accuracy = session.train()
+        test_accuracy = session.test()
+
+        hyper.update(test_accuracy, data.sample_size(DataType.TEST))
+        hyper.update_file_if_accuracy_improved()
+        logging.info("Valid Accuracy = {:.3f}".format(valid_accuracy))
+        logging.info('Test Accuracy = {:.3f}'.format(test_accuracy))
 
 
 if __name__ == "__main__":
--- CarND-Traffic-Sign-Classifier-Project/traffic_sign_detection/hyper_parameter_handler.py
+++ CarND-Traffic-Sign-Classifier-Project/traffic_sign_detection/hyper_parameter_handler.py
@@ -1,6 +1,7 @@
 """ Class to collect hyper parameter and optimize them """
 import json
 import logging
+import random
 from traffic_sign_detection.file_handler import FileHandler
 
 
@@ -10,7 +11,7 @@ class Parameters:
         self.learning_rate = tmp_dict['learning_rate']
         self.epochs = tmp_dict['epochs']
         self.batch_size = tmp_dict['batch_size']
-        self.accuracy = 0.0
+        self.accuracy = tmp_dict['accuracy']
 
     def to_dict(self):
         return {
@@ -19,6 +20,11 @@ class Parameters:
             'batch_size': self.batch_size,
             'accuracy': self.accuracy
         }
+
+    def add_random_offset(self):
+        self.learning_rate *= 10.0/random.randint(5, 15)
+        self.epochs += random.randint(-5, 5)
+        self.batch_size += random.randint(-20, 20)
 
 
 class HyperParameterHandler:
@@ -40,6 +46,22 @@ class HyperParameterHandler:
     @property
     def is_accuracy_improved(self):
         return self._is_accuracy_improved
+
+    def next_parameter_set(self):
+        self._parameter = Parameters(self.__read_file())
+        self._parameter.add_random_offset()
+        self.print_parameters()
+
+    def print_parameters(self):
+        self.logger.info("\n\t- accuracy=%.2f"
+                         "\n\t- learning_rate=%.4f"
+                         "\n\t- epochs=%d"
+                         "\n\t- batch_size=%d"
+                         ,
+                         self.parameter.accuracy,
+                         self.parameter.learning_rate,
+                         self.parameter.epochs,
+                         self.parameter.batch_size)
 
     def update(self, new_accuracy, sample_size):
         """ Update only if rule of 30 is satisfied """
