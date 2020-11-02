67d4b159f59b20ec6ad22717d77cdaac4e23b25f
Valentino Behret <valentino.b.behret@partner.bmw.de>
2020-11-02 18:18:01 +0100

    master: Implement abort mechanism if the accuracy does not increase

--- CarND-Traffic-Sign-Classifier-Project/traffic_sign_detection/session_handler.py
+++ CarND-Traffic-Sign-Classifier-Project/traffic_sign_detection/session_handler.py
@@ -2,6 +2,7 @@
 import logging
 import math
 
+import numpy as np
 import tensorflow as tf
 from sklearn.utils import shuffle
 from tqdm import tqdm
@@ -25,16 +26,18 @@ class SessionHandler:
         self.batch_size = hyper.parameter.batch_size
         self.epochs = hyper.parameter.epochs
         self.file = files.model_file
+        self._logger = logging.getLogger(SessionHandler.__name__)
 
     def train(self):
         feature_train, label_train = self.data.get_shuffled_data(DataType.TRAIN)
         batch_count = int(math.ceil(len(feature_train) / self.batch_size))
 
         saver = tf.train.Saver()
+        list_accuracy = np.array([])
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
 
-            logging.info("Training...")
+            self._logger.info("Training...")
             accuracy = 0.0
             for i in range(self.epochs):
 
@@ -47,30 +50,20 @@ class SessionHandler:
                 for batch_i in batches_pbar:
                     batch_start = batch_i * self.batch_size
                     batch_end = batch_start + self.batch_size
-                    batch_x = feature_train[batch_start:batch_end]
-                    batch_y = label_train[batch_start:batch_end]
                     sess.run(self.dnn.optimizer, feed_dict={
-                        self.dnn.tf_features: batch_x,
-                        self.dnn.tf_labels: batch_y,
+                        self.dnn.tf_features: feature_train[batch_start:batch_end],
+                        self.dnn.tf_labels: label_train[batch_start:batch_end],
                         self.dnn.tf_keep_prob: 0.5
                     })
 
-                    '''
-                    # Log every 50 batches
-                    if not batch_i % log_batch_step:
-                        # Calculate Training and Validation accuracy
-                        training_accuracy = sess.run(accuracy, feed_dict=train_feed_dict)
-                        validation_accuracy = sess.run(accuracy, feed_dict=valid_feed_dict)
-
-                        # Log batches
-                        previous_batch = batches[-1] if batches else 0
-                        batches.append(log_batch_step + previous_batch)
-                        loss_batch.append(l)
-                        train_acc_batch.append(training_accuracy)
-                        valid_acc_batch.append(validation_accuracy)
-                    '''
-
                 accuracy = self.validate()
+                list_accuracy = np.append(list_accuracy, accuracy)
+
+                if len(list_accuracy) > 3:
+                    mean_diff = np.mean(np.diff(list_accuracy[-3:]))
+                    if mean_diff < 0.01:
+                        self._logger.info("Abort, accuracy did not increase enough!")
+                        break
 
             saver.save(sess, self.file)
             print("Model saved")
