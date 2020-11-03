f9e00518e0e551013d84ab5c660a7e16b36c6ed8
behretv <valentino.behret@mailbox.org>
2020-11-03 21:04:58 +0100

    master: Refactoring generate network function

--- CarND-Traffic-Sign-Classifier-Project/traffic_sign_detection/convolutional_neural_network.py
+++ CarND-Traffic-Sign-Classifier-Project/traffic_sign_detection/convolutional_neural_network.py
@@ -10,12 +10,7 @@ class ConvolutionalNeuralNetwork:
 
     def __init__(self, data: DataHandler, hyper: HyperParameterHandler):
         # Properties which can be assigned by the input
-        self.__img_depth = data.image_depth
-        self.__img_shape = data.image_shape
-        self.tf_features = tf.placeholder(tf.float32, shape=data.image_shape)
-        self.tf_labels = tf.placeholder(tf.int32, None)
-        self.tf_keep_prob = tf.placeholder(tf.float32)
-        self.__tf_one_hot_labels = tf.one_hot(self.tf_labels, data.n_labels)
+        self.__data = data
         self.__learning_rate = hyper.parameter.learning_rate
 
         # Properties which have to be computed
@@ -23,17 +18,15 @@ class ConvolutionalNeuralNetwork:
         self.__logits = None
         self.__optimizer = None
 
-        self.mu = 0
-        self.sigma = 0.1
-
-        # Dimensions per layer:
-        # Input: 32x32x3
-        # Layer 1 conv: 5x5x3x6  --> ceil(32 - 5 + 1)/1 = 28
-        # Layer 1 pool: 5x5x6x16  --> 28/2 = 14
-        # Layer 2 conv: 5x5x3x6  --> ceil(14 - 5 + 1)/1 = 10
-        # Layer 2 pool: 5x5x6x16  --> 10/2 = 5
+        self.__mu = 0
+        self.__sigma = 0.1
         self.__depth = [6, 16]
         self.__filter_size = 5
+
+        self.tf_features = tf.placeholder(tf.float32, shape=data.image_shape)
+        self.tf_labels = tf.placeholder(tf.int32, None)
+        self.tf_keep_prob = tf.placeholder(tf.float32)
+        self.__tf_one_hot_labels = tf.one_hot(self.tf_labels, data.n_labels)
 
     @property
     def optimizer(self):
@@ -58,50 +51,52 @@ class ConvolutionalNeuralNetwork:
     def generate_network(self):
         # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
         # Pooling. Input = 28x28x6. Output = 14x14x6.
-        out_sz1 = np.ceil(float(self.__img_shape[1] - self.__filter_size + 1)/float(2))
-        layer_1 = self.__convolutional_layer(tf_feature=self.tf_features,
-                                             img_sz=self.__img_shape[1],
-                                             in_depth=self.__img_depth,
-                                             out_depth=6)
+        layer_1 = self.__convolutional_layer(layer=self.tf_features, out_depth=6)
 
         # Layer 2: Convolutional. Output = 10x10x16.
         # Pooling. Input = 10x10x16. Output = 5x5x16.
-        layer_2 = self.__convolutional_layer(tf_feature=layer_1,
-                                             img_sz=14,
-                                             in_depth=6,
-                                             out_depth=16)
+        layer_2 = self.__convolutional_layer(layer=layer_1, out_depth=16)
 
         # Flatten. Input = 5x5x16. Output = 400.
         fc = flatten(layer_2)
 
         # Layer 3: Fully Connected. Input = 400. Output = 120.
-        layer_3 = self.__fully_connected_layer(tf_feature=fc, in_sz=400, out_sz=120)
+        layer_3 = self.__fully_connected_layer(layer=fc, out_sz=120)
 
         # Layer 4: Fully Connected. Input = 120. Output = 84.
-        layer_4 = self.__fully_connected_layer(tf_feature=layer_3, in_sz=120, out_sz=84)
+        layer_4 = self.__fully_connected_layer(layer=layer_3, out_sz=84)
 
         # Layer 5: Fully Connected. Input = 84. Output = 43.
-        out_weight = tf.Variable(tf.truncated_normal(shape=(84, 43), mean=self.mu, stddev=self.sigma))
-        out_bias = tf.Variable(tf.zeros(shape=(1, 43)))
-        self.__logits = tf.matmul(layer_4, out_weight) + out_bias
+        self.__logits = self.__output_layer(layer=layer_4)
 
-    def __fully_connected_layer(self, tf_feature, in_sz, out_sz):
-        weight = tf.Variable(tf.truncated_normal(shape=(in_sz, out_sz), mean=self.mu, stddev=self.sigma))
+    def __output_layer(self, layer):
+        shape = (int(layer.shape[1]), self.__data.n_labels)
+        out_weight = tf.Variable(tf.truncated_normal(shape=shape, mean=self.__mu, stddev=self.__sigma))
+        out_bias = tf.Variable(tf.zeros(shape=(1, shape[1])))
+        return tf.matmul(layer, out_weight) + out_bias
+
+    def __fully_connected_layer(self, layer, out_sz):
+        shape = (int(layer.shape[1]), out_sz)
+        weight = tf.Variable(tf.truncated_normal(shape=shape, mean=self.__mu, stddev=self.__sigma))
         bias = tf.Variable(tf.zeros(shape=(1, out_sz)))
 
-        layer = tf.matmul(tf_feature, weight) + bias
+        layer = tf.matmul(layer, weight) + bias
         layer = tf.nn.relu(layer)
         return tf.nn.dropout(layer, keep_prob=self.tf_keep_prob)
 
-    def __convolutional_layer(self, tf_feature, img_sz, in_depth, out_depth):
+    def __convolutional_layer(self, layer, out_depth):
         strides = [1, 1, 1, 1]
+        img_sz = int(layer.shape[1])
+        img_depth = int(layer.shape[3])
         out_sz = np.ceil(float(img_sz - self.__filter_size + 1) / float(strides[1]))
 
-        weight = tf.Variable(tf.truncated_normal(shape=(self.__filter_size, self.__filter_size, in_depth, out_depth),
-                                                 mean=self.mu, stddev=self.sigma))
-        bias = tf.Variable(tf.zeros(shape=(1, out_sz, out_sz, out_depth)))
+        shape_weight = (self.__filter_size, self.__filter_size, img_depth, out_depth)
+        shape_bias = (1, out_sz, out_sz, out_depth)
 
-        conv_layer = tf.nn.conv2d(tf_feature, weight, strides=strides, padding='VALID') + bias
+        weight = tf.Variable(tf.truncated_normal(shape=shape_weight, mean=self.__mu, stddev=self.__sigma))
+        bias = tf.Variable(tf.zeros(shape=shape_bias))
+
+        conv_layer = tf.nn.conv2d(layer, weight, strides=strides, padding='VALID') + bias
         conv_layer = tf.nn.relu(conv_layer)
 
         k = [1, 2, 2, 1]
