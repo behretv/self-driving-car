5b23ce11b43c04713d83489bcdd481c42a2bbc03
Valentino Behret <valentino.b.behret@partner.bmw.de>
2020-11-04 14:43:23 +0100

    master: Refactor testing

--- CarND-Traffic-Sign-Classifier-Project/__main__.py
+++ CarND-Traffic-Sign-Classifier-Project/__main__.py
@@ -1,4 +1,5 @@
 import logging
+import coloredlogs
 
 from traffic_sign_detection.data_handler import DataHandler, DataType
 from traffic_sign_detection.convolutional_neural_network import ConvolutionalNeuralNetwork
@@ -7,8 +8,10 @@ from traffic_sign_detection.hyper_parameter_handler import HyperParameterHandler
 from traffic_sign_detection.session_handler import SessionHandler
 
 
-def main():
+def train():
     logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    coloredlogs.install(level='INFO', logger=logger)
 
     # 1 Data and hyper-parameter
     files = FileHandler()
@@ -17,9 +20,10 @@ def main():
     data.process()
     #data.visualize_labels_histogram()
     hyper = HyperParameterHandler(files)
+    session = SessionHandler(files, data)
 
-    for i in range(0, 10):
-        logging.info("%d # ITERATION \n\n", i)
+    for i in range(0, 3):
+        logger.info("%d # ITERATION \n\n", i)
         hyper.next_parameter_set()
 
         # 2 DNN
@@ -29,15 +33,16 @@ def main():
         covnet.compute_cost()
 
         # 3 Run and save session
-        session = SessionHandler(files, data, covnet, hyper)
-        valid_accuracy, sess = session.train()
-        test_accuracy = session.test()
-
-        hyper.update(test_accuracy, data.sample_size(DataType.TEST))
+        session.cnn = covnet
+        session.params = hyper.parameter
+        valid_accuracy = session.train(i)
+        logger.info("Valid Accuracy = {:.3f}".format(valid_accuracy))
+        hyper.update(valid_accuracy, data.sample_size(DataType.TEST))
         hyper.update_file_if_accuracy_improved()
-        logging.info("Valid Accuracy = {:.3f}".format(valid_accuracy))
-        logging.info('Test Accuracy = {:.3f}'.format(test_accuracy))
+
+        test_accuracy = session.test(i)
+        logger.info('Test Accuracy = {:.3f}'.format(test_accuracy))
 
 
 if __name__ == "__main__":
-    main()
+    train()
--- CarND-Traffic-Sign-Classifier-Project/traffic_sign_detection/session_handler.py
+++ CarND-Traffic-Sign-Classifier-Project/traffic_sign_detection/session_handler.py
@@ -18,57 +18,91 @@ class SessionHandler:
     def __init__(self,
                  files: FileHandler,
                  data: DataHandler,
-                 cnn: ConvolutionalNeuralNetwork,
-                 hyper: HyperParameterHandler):
+                # cnn: ConvolutionalNeuralNetwork,
+                #hyper: HyperParameterHandler
+                 ):
 
         self.__data = data
-        self.__cnn = cnn
-        self.__batch_size = hyper.parameter.batch_size
-        self.__epochs = hyper.parameter.epochs
+        self.__cnn = None  # cnn
+        self.__params = None  #hyper.parameter
         self.__file = files.model_file
         self.__logger = logging.getLogger(SessionHandler.__name__)
         self.__session = None
+        self.__saver = None
+        
+    @property
+    def cnn(self):
+        assert self.__cnn is not None
+        return self.__cnn
+    
+    @cnn.setter
+    def cnn(self, value):
+        assert value is not None
+        self.__cnn = value
 
-    def train(self):
+    @property
+    def params(self):
+        assert self.__params is not None
+        return self.__params
+
+    @params.setter
+    def params(self, value):
+        assert value is not None
+        self.__params = value
+
+    def train(self, step):
         feature_train, label_train = self.__data.get_shuffled_data(DataType.TRAIN)
-        batch_count = int(math.ceil(len(feature_train) / self.__batch_size))
+        batch_count = int(math.ceil(len(feature_train) / self.__params.batch_size))
 
-        saver = tf.train.Saver()
+        self.__saver = tf.train.Saver()
         list_accuracy = np.array([])
         with tf.Session() as self.__session:
+            self.__logger.info("{}# Training...".format(step))
             self.__session.run(tf.global_variables_initializer())
 
-            self.__logger.info("Training...")
-            accuracy = 0.0
-            for i in range(self.__epochs):
-
+            for i in range(self.params.epochs):
                 # Progress bar
-                batches_pbar = tqdm(range(batch_count),
-                                    desc='Previous Accuracy={:.3f} Epoch {:>2}/{}'.format(accuracy, i + 1, self.__epochs),
-                                    unit='batches')
+
+                batches_pbar = tqdm(range(batch_count), desc=self.progress_msg(i, list_accuracy), unit='batches')
 
                 feature_train, label_train = shuffle(feature_train, label_train)
                 for batch_i in batches_pbar:
-                    batch_start = batch_i * self.__batch_size
-                    batch_end = batch_start + self.__batch_size
-                    sess.run(self.__cnn.optimizer, feed_dict={
-                        self.__cnn.tf_features: feature_train[batch_start:batch_end],
-                        self.__cnn.tf_labels: label_train[batch_start:batch_end],
-                        self.__cnn.tf_keep_prob: 0.5
+                    batch_start = batch_i * self.params.batch_size
+                    batch_end = batch_start + self.params.batch_size
+                    self.__session.run(self.cnn.optimizer, feed_dict={
+                        self.cnn.tf_features: feature_train[batch_start:batch_end],
+                        self.cnn.tf_labels: label_train[batch_start:batch_end],
+                        self.cnn.tf_keep_prob: 0.5
                     })
 
-                accuracy = self.validate()
-                list_accuracy = np.append(list_accuracy, accuracy)
+                list_accuracy = np.append(list_accuracy, self.validate())
+                if not self.is_accuracy_improved(list_accuracy):
+                    break
 
-                if len(list_accuracy) > 3:
-                    mean_diff = np.mean(np.diff(list_accuracy[-4:]))
-                    if mean_diff < 0.005:
-                        self.__logger.info("Abort, accuracy did not increase enough!")
-                        break
+            self.save_model(step)
 
-            saver.save(sess, self.__file)
-            print("Model saved")
-        return accuracy, sess
+        return list_accuracy[-1]
+
+    def progress_msg(self, i, list_accuracy):
+        if i > 0:
+            msg = 'Previous Accuracy={:.3f} Epoch {:>2}/{}'.format(list_accuracy[i - 1], i + 1, self.params.epochs)
+        else:
+            msg = 'Epoch {:>2}/{}'.format(i + 1, self.params.epochs)
+        return msg
+
+    def is_accuracy_improved(self, list_accuracy):
+        is_improved = True
+        if len(list_accuracy) > 3:
+            mean_diff = np.mean(np.diff(list_accuracy[-4:]))
+            if mean_diff < 0.005:
+                self.__logger.info("Abort, accuracy did not increase enough!")
+                is_improved = False
+        return is_improved
+
+    def save_model(self, step):
+        filename = self.__file + str(step)
+        self.__logger.info("Save model as: {}".format(filename))
+        self.__saver.save(self.__session, filename)
 
     def validate(self):
         feature_valid, label_valid = self.__data.get_shuffled_data(DataType.VALID)
@@ -76,28 +110,31 @@ class SessionHandler:
 
         total_accuracy = 0
         tmp_sess = tf.get_default_session()
-        for i_start in range(0, n_features, self.__batch_size):
-            i_end = i_start + self.__batch_size
+        for i_start in range(0, n_features, self.params.batch_size):
+            i_end = i_start + self.params.batch_size
             tmp_features = feature_valid[i_start:i_end]
             tmp_labels = label_valid[i_start:i_end]
-            tmp_accuracy = tmp_sess.run(self.__cnn.cost, feed_dict={
-                self.__cnn.tf_features: tmp_features,
-                self.__cnn.tf_labels: tmp_labels,
-                self.__cnn.tf_keep_prob: 1.0
+            tmp_accuracy = tmp_sess.run(self.cnn.cost, feed_dict={
+                self.cnn.tf_features: tmp_features,
+                self.cnn.tf_labels: tmp_labels,
+                self.cnn.tf_keep_prob: 1.0
             })
             total_accuracy += (tmp_accuracy * len(tmp_features))
         return total_accuracy / n_features
 
-    def test(self):
+    def test(self, step):
+        filename = self.__file + str(step)
+        self.__logger.info("Restore model: {}".format(filename))
+
         # Runs saved session
         saver = tf.train.Saver()
         feature_test, label_test = self.__data.get_shuffled_data(DataType.TEST)
         with tf.Session() as sess:
-            saver.restore(sess, self.__file)
-            test_accuracy = sess.run(self.__cnn.cost, feed_dict={
-                self.__cnn.tf_features: feature_test,
-                self.__cnn.tf_labels: label_test,
-                self.__cnn.tf_keep_prob: 1.0
+            saver.restore(sess, filename)
+            test_accuracy = sess.run(self.cnn.cost, feed_dict={
+                self.cnn.tf_features: feature_test,
+                self.cnn.tf_labels: label_test,
+                self.cnn.tf_keep_prob: 1.0
             })
 
         return test_accuracy
