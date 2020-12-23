5c1343bff7e28371dc2fe160fd9fdf10990280fa
Valentino Behret <valentino.b.behret@partner.bmw.de>
2020-12-23 17:59:19 +0100

    P5-Kidnapped-Vehicle: Unit test - Init

--- CarND-Kidnapped-Vehicle-P5/tst/particle_filter_test.cpp
+++ CarND-Kidnapped-Vehicle-P5/tst/particle_filter_test.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "../src/particle_filter.h"
 
-TEST(GetAssociations_Test, test1) {
+TEST(GetAssociationsTest, TestPositiveVectors) {
     //arrange
     //act
     //assert
@@ -9,4 +9,11 @@ TEST(GetAssociations_Test, test1) {
     p.associations = {1, 2, 3};
     ParticleFilter pf = ParticleFilter();
     EXPECT_EQ(pf.getAssociations(p),  "1 2 3");
+}
+
+TEST(InitTest, TestNumberOfParticles){
+    ParticleFilter filter = ParticleFilter();
+    double std_pos[3] = {1.0, 2.0, 3.0};
+    filter.init(0.0, 0.0, 0.0, std_pos);
+    EXPECT_EQ(filter.particles.size(), 1);
 }
